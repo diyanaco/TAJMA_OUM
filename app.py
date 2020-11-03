8f4bcce15713d03e56a5b5a03248e44dde10c292
miazocool <zaimsaharudin@gmail.com>
2020-11-03 21:55:56 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import request
+
+# from flask_sqlalchemy import SQLAlchemy 
+# import sqlalchemy as db
+
+# engine = db.create_engine('mysql://root:miazocool1503@localhost/crud')
+# connection = engine.connect()
+# metadata = db.MetaData()
+# census = db.Table('student', metadata, autoload=True, autoload_with=engine)
+
+# project_dir = os.path.dirname(os.path.abspath(__file__))
+# database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+# db = SQLAlchemy(app)
+
+# class Book(db.Model):
+#     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+#     def __repr__(self):
+#         return "<Title: {}>".format(self.title)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+#     if request.form:
+#         print(request.form)  
+    return render_template("index.html")
+    #return "Hello World Kih"
+
+if __name__ == "__main__":
+    app.run(debug=True)
