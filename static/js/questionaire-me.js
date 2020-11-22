fad061f12f67cd9baa29a7573526022c76b47970
miazocool <zaimsaharudin@gmail.com>
2020-11-22 17:03:57 +0800

    modified questionaire-me

--- static/js/questionaire-me.js
+++ static/js/questionaire-me.js
@@ -9,13 +9,34 @@
 //     });
 //   });
 
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      if($(this).scrollTop() > 100){
-        $(".question").css({"opacity" : "0"})
+  // $(document).ready(function(){
+  //   $(window).scroll(function(){
+  //     if($(this).scrollTop() > 100){
+  //       $(".question").css({"opacity" : "0"})
+  //     }
+  //     else {
+  //       $(".question").css({"opacity" : "1"})
+  //     }
+  //   })
+  // })
+
+  $(window).on("scroll", function() {
+    $(container).find(".question").each(function() {
+      var etop = $(this).offset().top;
+      var diff = etop - $(window).scrollTop();
+
+      if (diff > 100 && diff < 300) {
+        reinitState(this);
       }
-      else {
-        $(".question").css({"opacity" : "1"})
-      }
-    })
-  })
+    });
+  });
+
+  function reinitState(e) {
+    $(container).find(".ques").removeClass("active");
+
+    $(container).find(".ques").each(function() {
+      $(this).blur();
+    });
+    $(e).addClass("active");
+    /*$(e).find('input').focus();*/
+  }
