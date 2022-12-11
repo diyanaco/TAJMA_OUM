a559c19a4c761e44be1b0183476f85e267c0708d
sayasaya <zaimsaharudin@gmail.com>
2022-12-11 19:35:53 +0800

    Added sql scripts folder

---
+++ sql/insert-init.sql
@@ -0,0 +1,25 @@
+-- SQLite
+INSERT into psy_student (
+        id,
+        firstName,
+        lastName,
+        email,
+        profPic,
+        gender,
+        age,
+        IC,
+        race,
+        mobile
+    )
+VALUES (
+        '8e5a2a32-4f77-473b-af85-a29da3390688',
+        'muhammad',
+        'zaim',
+        'zaim@test.com',
+        'zaimprofilepicture.jpd',
+        'male',
+        '21',
+        '910321031111',
+        'malay',
+        '01299999'
+    )
