262858cce331dd922cb2e02b1d4a0ac384a044af
sayasaya <zaimsaharudin@gmail.com>
2022-12-31 15:56:47 +0800

    Added availability form for counselor profile

--- tajma/forms/CalendarAvailabilityForm.py
+++ tajma/forms/CalendarAvailabilityForm.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField
+from wtforms import RadioField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms.fields.html5 import TimeField
 
+
 class CalendarAvailabilityForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    # name = StringField('name', validators=[DataRequired()])
     days = RadioField(
-        'days', choices=["Monday", "Tuesday", "Wednesday"], validators=[DataRequired()])
+        'days', choices=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], validators=[DataRequired()])
     session_start = TimeField("Start", format='%H:%M')
     session_end = TimeField("End", format='%H:%M')
+    submit = SubmitField('Update')
+
