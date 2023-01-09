6d6c164f8ad43eb0e9cd2b3e98d618061034a627
sayasaya <zaimsaharudin@gmail.com>
2023-01-10 03:31:09 +0800

    Remove not null constraints

--- models/UserModel.py
+++ models/UserModel.py
@@ -7,59 +7,72 @@ from .UserRoleLinkModel import association_user_role_table
 from .UserCalendarEventModel import association_user_calendar_event_table
 from . import Base
 
+
 class User(Base, UserMixin):
     __tablename__ = "psy_user"
     id = Column(String(50), primary_key=True)
-    firstName = Column(String(50), nullable=True)
-    lastName = Column(String(50), nullable=False)
+    firstName = Column(String(50))
+    lastName = Column(String(50))
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
-    gender = Column(String(50), nullable=False)
-    age = Column(String(50), nullable=False)
-    profPic = Column(String(50), nullable=False, default='default.jpg')
-    IC = Column(String(50), nullable=False)
-    race = Column(String(50), nullable=False)
-    mobile = Column(String(50), nullable=False)
+    gender = Column(String(50))
+    age = Column(String(50))
+    profPic = Column(String(50), default='default.jpg')
+    IC = Column(String(50))
+    race = Column(String(50))
+    mobile = Column(String(50))
     # active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
     email_confirmed_at = Column(DateTime)
-    elearningTaken = Column(Boolean, nullable =True)
-    learnerTaken = Column(Boolean, nullable =True)
-    attitudeTaken = Column(Boolean, nullable =True)
-    salut = Column(String(50), nullable=True)
-    revision = Column(String(50), nullable=True)
-    #To remove
-    test = Column(String(50), nullable=True)
-    #if s means its one to many, else one to one
+    elearningTaken = Column(Boolean)
+    learnerTaken = Column(Boolean)
+    attitudeTaken = Column(Boolean)
+    salut = Column(String(50))
+    revision = Column(String(50))
+    # To remove
+    test = Column(String(50))
+    # if s means its one to many, else one to one
     roles = relationship("Role", secondary=association_user_role_table)
-    calendar_event = relationship("CalendarEvent", secondary=association_user_calendar_event_table)
+    calendar_event = relationship(
+        "CalendarEvent", secondary=association_user_calendar_event_table)
 
     active = Column(Boolean())
-    
-    #override get_id method from UserMixin
+
+    # override get_id method from UserMixin
     def get_id(self):
         return self.id
+
     def get_first_name(self):
         return self.firstName
+
     def get_last_name(self):
         return self.lastName
+
     def get_email(self):
         return self.email
+
     def get_gender(self):
         return self.gender
+
     def get_age(self):
         return self.age
+
     def get_profPic(self):
         return self.profPic
+
     def get_IC(self):
         return self.IC
+
     def get_race(self):
         return self.race
+
     def get_mobile(self):
         return self.mobile
+
     def gen_full_name(self):
-        return self.firstName +" "+ self.lastName
+        return self.firstName + " " + self.lastName
+
     def gen_salutation(self):
-        return self.salut +" "+ self.gen_full_name()
+        return self.salut + " " + self.gen_full_name()
 
     def __repr__(self):
         return f"User('{self.email}', '{self.password}', {self.id})"
