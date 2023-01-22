713f71f935f14f2518b0dc3d360c8c1ca739eb9b
sayasaya <zaimsaharudin@gmail.com>
2023-01-22 11:17:25 +0800

    Able to insert into slot with date format

--- models/SlotModel.py
+++ models/SlotModel.py
@@ -1,22 +1,26 @@
-from sqlalchemy import Column, String, DateTime, Integer, Float
+from sqlalchemy import Column, String, DateTime, Integer, Float, Time
 from . import Base
 
-class Slot(Base):
-    __tablename__="psy_slot"
-    id = Column(String(50), primary_key=True)
 
-    #If end slot is emptied, it means a one time meeting,
+class Slot(Base):
+    __tablename__ = "psy_slot"
+
+    def __init__(self, id, start_slot, end_slot):
+        self.id = id
+        self.start_slot = start_slot
+        self.end_slot = end_slot
+
+    id = Column(String(50), primary_key=True)
+    # If end slot is emptied, it means a one time meeting,
     # else it will be a recurring meeting
-    start_slot = Column("start_slot", DateTime)
-    end_slot = Column("end_slot", DateTime)
-    
+    start_slot = Column("start_slot", Time)
+    end_slot = Column("end_slot", Time)
     hours = Column("hours", Float)
     # AVAILABILITY
     # BREAK
     # SESSION
-    # BREAK_BETWEEN_SESSION 
+    # BREAK_BETWEEN_SESSION
     type = Column(String(50))
 
     def __repr__(self):
         return f"Slot('{self.id}', '{self.start_slot}', {self.end_slot})"
-
